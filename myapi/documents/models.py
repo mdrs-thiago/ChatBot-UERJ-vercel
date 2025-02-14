9288417e6c517f6ad690c4457f567243c261d57a
Tiago Souza <tiagosouza.eletrotecnico@gmail.com>
2025-02-13 21:30:46 -0300

    content can be null

--- myapi/documents/models.py
+++ myapi/documents/models.py
@@ -3,7 +3,7 @@ from django.db import models
 class Document(models.Model):
     title = models.CharField(max_length=255)
     file = models.FileField(upload_to="documents/")
-    content = models.TextField(blank=True)
+    content = models.TextField(blank=True, null=True)
     uploaded_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
