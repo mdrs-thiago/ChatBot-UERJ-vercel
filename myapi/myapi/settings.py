d333ad9dc56e54a36fab51eb873fdb0e99609f10
Thiago Medeiros <thiago.medeiros.carvalho@gmail.com>
2026-05-25 08:50:38 -0300

    fix staticfiles

--- myapi/myapi/settings.py
+++ myapi/myapi/settings.py
@@ -140,7 +140,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = "static/"
-STATIC_ROOT = BASE_DIR / "staticfiles"
+STATIC_ROOT = REPO_ROOT / "staticfiles"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
